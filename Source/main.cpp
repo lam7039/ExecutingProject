4014eb9d52627cef35b540008712980e1ad57498
lamram <kelvin7039@gmail.com>
2017-12-05 18:10:27 +0100

    moved the object creation to this project

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,44 @@
 #include <Windows.h>
 #include "Engine.h"
+#include "SceneLoader.h"
+
+class Airplane : public se::Object {
+public:
+	Airplane();
+	virtual void Render();
+};
+
+Airplane::Airplane() {
+	Init("airplane.x");
+	SetPosition(2.0f, 0.0f, -5.0f);
+}
+
+void Airplane::Render() {
+	se::LogDebug("testairplane");
+}
+
+class Tiger : public se::Object {
+public:
+	Tiger();
+};
+
+Tiger::Tiger() {
+	Init("tiger.x");
+	SetPosition(-2.0f, -0.0f, 0.0f);
+}
 
 void Start() {
+	Airplane *airplane = new Airplane();
+	Tiger *tiger = new Tiger();
+
+	se::SceneLoader::GetInstance()->AddScene("rotatingobjects");
+	se::SceneLoader::GetInstance()->GetScene("rotatingobjects")->AddObject(airplane);
+	se::SceneLoader::GetInstance()->GetScene("rotatingobjects")->AddObject(tiger);
+	se::SceneLoader::GetInstance()->AddScene("heightmap");
+	se::SceneLoader::GetInstance()->GetScene("heightmap")->AddObject(tiger);
+	se::SceneLoader::GetInstance()->SetCurrentScene("rotatingobjects");
 	se::LogDebug("teststart");
+	se::SceneLoader::GetInstance()->GetScene("rotatingobjects")->RemoveObject(tiger);
 }
 
 void Stop() {
