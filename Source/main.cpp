e53f939e3930b36907add0f5112b3232b945d3ba
lamram <kelvin7039@gmail.com>
2018-01-17 01:57:43 +0100

    added proper skybox

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,13 +1,14 @@
 #include "Kernel.h"
-#include "Window.h"
 #include "SceneManager.h"
 #include "Timer.h"
 #include "AssetLoader.h"
+#include "Transform.h"
 #include "Input.h"
 #include "CameraController.h"
 #include "DirectX9/DirectXTerrain.h"
 #include "DirectX9/DirectXMesh.h"
 #include "DirectX9/DirectXCamera.h"
+#include "DirectX9/DirectXSkybox.h"
 
 //MACROAGRESSION DETECTED
 #define PI 3.141592
@@ -20,10 +21,11 @@
 //};
 //
 //Skybox::Skybox() {
-//	m_entityName = "skybox2.x";
+//	m_entityName = "skybox";
+//	m_assetName = "skybox";
 //	SetPosition(2.0f, 0.0f, -5.0f);
 //	SetRotation(PI, -(PI / 2), 0.0f);
-//	SetScale(2.0f);
+//	SetScale(2.0f, 2.0f, 2.0f);
 //}
 //
 //void Skybox::Render() {
@@ -41,7 +43,7 @@ private:
 Tiny::Tiny() {
 	m_entityName = "asdf";
 	m_assetName = "tiny";
-	SetScale(1.0f / 50);
+	SetScale(1.0f / 50, 1.0f / 50, 1.0f / 50);
 	SetPosition(-2.0f, 0.0f, 0.0f);
 	//SetRotation(0.0f, -(PI / 2), 0.0f);
 	m_timer.Start();
@@ -52,29 +54,37 @@ void Tiny::Update(float delta) {
 	SetRotation(iTime * (2.0f * PI) / 1000.0f, -(PI / 2), 0.0f);
 }
 
-int main(int argc, char **argv) {
+int main() {
 	se::Input input;
 	se::Kernel kernel("GameEngine3D", 800, 500, new se::Direct3D(), &input);
 	//kernel.AddWindow("test", 1920, 1080);
 	se::Debug logger("project.log");
 	logger.Log(0, __FILE__, __LINE__, "project started");
 
+	//se::Transform3f target;
+	//target.posZ = -15.0f;
 	
 	se::CameraController controller(new se::DirectXCamera(), &input);
-
+	kernel.AddCameraController(&controller);
+	//controller.SetTarget(&target);
 
 	se::Mesh *tinyMesh = new se::Mesh("Assets\\tiny.x");
+	Tiny *tiny = new Tiny();
+	//se::Mesh *skyboxMesh = new se::Mesh("Assets\\skybox2.x");
+	//Skybox *skybox = new Skybox();
 
-	//se::AssetLoader::GetInstance()->AddMesh("skybox2.x");
+	//se::AssetLoader::GetInstance()->AddAsset("skybox", skyboxMesh);
 	se::AssetLoader::GetInstance()->AddAsset("tiny", tinyMesh);
 
 	se::Terrain *terrain = new se::Terrain();
 	terrain->Create("Heightmap2.bmp", "texture.jpg");
-	//Skybox *skybox = new Skybox();
-	Tiny *tiny = new Tiny();
+	se::Skybox *skybox = new se::Skybox();
+	se::Transform3f test;
+	skybox->Create(test, "Assets\\skybox_texture.jpg");
 
 	se::SceneManager::GetInstance()->AddScene("world");
-	se::SceneManager::GetInstance()->GetScene("world")->SetTerrain(terrain);
+	//se::SceneManager::GetInstance()->GetScene("world")->SetTerrain(terrain);
+	se::SceneManager::GetInstance()->GetScene("world")->SetSkybox(skybox);
 	//se::SceneManager::GetInstance()->GetScene("world")->AddEntity(skybox);
 	se::SceneManager::GetInstance()->GetScene("world")->AddEntity(tiny);
 	se::SceneManager::GetInstance()->SetCurrentScene("world");
